78c80b5701660ca5822703d867e0491d5675e829
Nikodem Okroj <n.okroj@icloud.com>
2025-01-22 17:11:17 +0100

    feat: enhance home page with username management functionality
    - Integrated Redux hooks to manage username state on the home page.
    - Added buttons for setting and resetting the username, improving interactivity.
    - Displayed the current username dynamically within the UI.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 "use client";
 
 import { Button } from "@heroui/react";
+import { useAppDispatch, useAppSelector } from '@/app/store/hooks';
+import { setUsername, resetUsername } from '@/app/store/features/authSlice';
 
 export default function Home() {
+  const username = useAppSelector((state) => state.auth.username);
+  const dispatch = useAppDispatch();
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <div className="text-4xl font-bold">Hello World</div>
-      <Button variant="solid">Click me</Button>
+      <div className="flex gap-4 items-center">
+        <Button variant="solid" onPress={() => dispatch(resetUsername())}>Reset Username</Button>
+        <span className="text-2xl">{username}</span>
+        <Button variant="solid" onPress={() => dispatch(setUsername('JohnDoe'))}>Set Username</Button>
+      </div>
     </div>
   );
 }
