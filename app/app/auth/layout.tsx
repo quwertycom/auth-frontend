cf50fa35b2751f0a706f455216ebf101f5427a26
Nikodem Okroj <n.okroj@icloud.com>
2025-01-27 11:51:40 +0100

    chore: removed auth layout text from auth layout

--- app/app/auth/layout.tsx
+++ app/app/auth/layout.tsx
@@ -2,7 +2,6 @@
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1>Auth Layout</h1>
       {children}
     </div>
   );
