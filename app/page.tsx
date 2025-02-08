aef1adb0cc4cc7153e0c3edd776a6eaa70b10687
Nikodem Okroj <n.okroj@icloud.com>
2025-02-08 10:14:46 +0100

    chore: clean up home page template

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
 'use client';
 
-import { Link } from '@heroui/react';
-
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-16 p-8 pb-20 sm:p-20">
-      <h1 className="text-4xl font-bold">Hello World</h1>
-      <div className="flex items-center gap-4">
-        <Link href="/app/auth/login">Auth</Link>
-      </div>
     </div>
   );
 }
