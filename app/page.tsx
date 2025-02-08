e91a7b3f853204c69da9635f7264d5aa199eb7a1
Nikodem Okroj <n.okroj@icloud.com>
2025-02-08 10:25:27 +0100

    feat: add landing page with navbar and navigation buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,29 @@
 'use client';
 
+import { Button, Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/react";
+import MaterialSymbol from "./components/materialSymbol";
+import TransitionLink from "./components/transitionLink";
+import { useNavigateWithAnimation } from "./utils/NavigateWithAnimation";
+
 export default function Home() {
+  const navigate = useNavigateWithAnimation();
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-16 p-8 pb-20 sm:p-20">
+    <div className="w-screen h-screen">
+      <Navbar>
+        <NavbarBrand className="flex flex-row items-center gap-2">
+          <MaterialSymbol symbol="badge" size={32} />
+          <span className="text-2xl font-bold">qAuth</span>
+        </NavbarBrand>
+        <NavbarContent justify="end">
+          <NavbarItem> 
+            <TransitionLink href="/app/auth/login" color="foreground">Login</TransitionLink>
+          </NavbarItem>
+          <NavbarItem>
+            <Button color="primary" variant="flat" onPress={() => navigate({href: '/app/auth/register'})}>Register</Button>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
     </div>
   );
 }
