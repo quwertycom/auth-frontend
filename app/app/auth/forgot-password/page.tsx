4e32e9213f86233b2743aeb031df21b4e15fe549
Nikodem Okroj <n.okroj@icloud.com>
2025-02-09 11:08:23 +0100

    style: formatted code via prettier

--- app/app/auth/forgot-password/page.tsx
+++ app/app/auth/forgot-password/page.tsx
@@ -22,7 +22,9 @@ export default function ForgotPassword() {
 
   const [username, setUsername] = useState('');
 
-  const [status, setStatus] = useState<'idle' | 'loading' | 'success' | 'error'>('error');
+  const [status, setStatus] = useState<
+    'idle' | 'loading' | 'success' | 'error'
+  >('error');
   const nodeRef = useRef(null);
 
   const handleSubmit = () => {
@@ -30,7 +32,7 @@ export default function ForgotPassword() {
     setTimeout(() => {
       setStatus('success');
     }, 3000);
-  }
+  };
 
   const transition = centerPopTransition();
 
@@ -40,21 +42,19 @@ export default function ForgotPassword() {
     },
     animate: {
       ...transition.normal.enterTo,
-      transition:
-        transition.normal.enterActive
-          .transition,
+      transition: transition.normal.enterActive.transition,
     },
     exit: {
       ...transition.normal.leaveTo,
-      transition:
-        transition.normal.leaveActive
-          .transition,
+      transition: transition.normal.leaveActive.transition,
     },
-  }
+  };
 
   return (
     <div className="relative h-screen w-screen">
-      <div className={`relative flex h-screen w-screen flex-col items-center justify-center gap-8 ${status !== "idle" ? "scale-95 opacity-50 blur-sm" : "scale-100 opacity-100 blur-0"} transition-all duration-[350ms] ease-in-out-cubic`}>
+      <div
+        className={`relative flex h-screen w-screen flex-col items-center justify-center gap-8 ${status !== 'idle' ? 'scale-95 opacity-50 blur-sm' : 'scale-100 opacity-100 blur-0'} transition-all duration-[350ms] ease-in-out-cubic`}
+      >
         <Card className="w-full max-w-md">
           <CardHeader className="flex flex-row items-center gap-2">
             <MaterialSymbol symbol="help_outline" size={32} />
@@ -78,7 +78,12 @@ export default function ForgotPassword() {
             </p>
           </CardBody>
           <CardFooter>
-            <Button color="primary" variant="flat" className="w-full" onPress={handleSubmit}>
+            <Button
+              color="primary"
+              variant="flat"
+              className="w-full"
+              onPress={handleSubmit}
+            >
               Send reset link
             </Button>
           </CardFooter>
@@ -103,48 +108,67 @@ export default function ForgotPassword() {
               className="z-50 flex h-screen w-screen items-center justify-center"
             >
               {status === 'loading' ? (
-                <Card className='rounded-full'>
+                <Card className="rounded-full">
                   <CardBody>
                     <Spinner size="lg" />
                   </CardBody>
                 </Card>
               ) : status === 'success' ? (
-                <Card className='w-full max-w-xs'>
-                  <CardHeader className='flex flex-col items-center gap-4 text-success pt-8'>
-                    <MaterialSymbol symbol="check_circle" size={128} fill/>
-                    <span className='text-3xl font-bold'>Success!</span>
+                <Card className="w-full max-w-xs">
+                  <CardHeader className="flex flex-col items-center gap-4 pt-8 text-success">
+                    <MaterialSymbol symbol="check_circle" size={128} fill />
+                    <span className="text-3xl font-bold">Success!</span>
                   </CardHeader>
-                  <CardBody className='pb-8'>
-                    <p className='text-center text-sm'>
-                      We have sent a reset link to your email. Please check your inbox and spam folder. If you don&apos;t see it, please{' '}
-                      <TransitionLink href="/app/auth/login" className="text-sm">
+                  <CardBody className="pb-8">
+                    <p className="text-center text-sm">
+                      We have sent a reset link to your email. Please check your
+                      inbox and spam folder. If you don&apos;t see it, please{' '}
+                      <TransitionLink
+                        href="/app/auth/login"
+                        className="text-sm"
+                      >
                         contact our support
                       </TransitionLink>
                       .
                     </p>
                   </CardBody>
                   <CardFooter>
-                    <Button color="primary" variant="flat" className="w-full" onPress={() => navigate({href: '/app/auth/login'})}>
+                    <Button
+                      color="primary"
+                      variant="flat"
+                      className="w-full"
+                      onPress={() => navigate({ href: '/app/auth/login' })}
+                    >
                       Back to login
                     </Button>
                   </CardFooter>
                 </Card>
               ) : status === 'error' ? (
-                <Card className='w-full max-w-xs'>
-                  <CardHeader className='flex flex-col items-center gap-4 text-error pt-8 text-danger'>
-                    <MaterialSymbol symbol="error" size={128} fill/>
-                    <span className='text-3xl font-bold'>Error!</span>
+                <Card className="w-full max-w-xs">
+                  <CardHeader className="text-error flex flex-col items-center gap-4 pt-8 text-danger">
+                    <MaterialSymbol symbol="error" size={128} fill />
+                    <span className="text-3xl font-bold">Error!</span>
                   </CardHeader>
                   <CardBody>
-                    <p className='text-center text-sm'>Something went wrong while trying to sent reset link, please try again later. if issue persists, please{' '}
-                      <TransitionLink href="/app/auth/login" className="text-sm">
+                    <p className="text-center text-sm">
+                      Something went wrong while trying to sent reset link,
+                      please try again later. if issue persists, please{' '}
+                      <TransitionLink
+                        href="/app/auth/login"
+                        className="text-sm"
+                      >
                         contact our support
                       </TransitionLink>
                       .
                     </p>
                   </CardBody>
                   <CardFooter>
-                    <Button color="primary" variant="flat" className="w-full" onPress={() => setStatus('idle')}>
+                    <Button
+                      color="primary"
+                      variant="flat"
+                      className="w-full"
+                      onPress={() => setStatus('idle')}
+                    >
                       Close
                     </Button>
                   </CardFooter>
